2dab89e3816fb8949bc09660b630071a64015b97
ThanhTuan <thanhtuan090398@gmail.com>
2019-12-02 10:41:03 +0700

    update

--- MachineLearning/LASSO_Regression.py
+++ MachineLearning/LASSO_Regression.py
@@ -1,50 +1,88 @@
-import numpy as np
+import math 
+import matplotlib.pyplot as plt 
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+
+
+# difference of lasso and ridge regression is that some of the coefficients can be zero i.e. some of the features are 
+# completely neglected
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
-def gradient_descent(x,y):
-  lamda=2  
-  n_iter=500
-  m=len(x)
-  l_rate=0.0000001
-  er_thres=0.001  
-  j_list=[]
-  iter_list=[]
-  #y_plist=[]  
-  theta=[3,3,3,3,3]
-  theta=np.array(theta)
-  for i in range(n_iter):    
-    y_pred=(theta.T.dot(x.T))
-    #y_plist.append(y_pred)
-    j_theta=(np.sum((y-y_pred)**2))/(2*m)+lamda * np.sum(np.abs(theta))
-    j_list.append(j_theta)
-    iter_list.append(i)
-    grad_theta=(-2/m)*(sum(x.T.dot(y-y_pred)))
-    theta=theta-l_rate*grad_theta
-    if(j_theta<er_thres and (len(j_list)>10) and np.mean(j_list)):
-        break
-    elif(j_theta<er_thres):
-        break
-  return(iter_list,j_list,theta,y_pred)
+cancer = load_breast_cancer()
+
+#print cancer.keys()
+cancer_df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+
+#print cancer_df.head(3)
+X = cancer.data
+Y = cancer.target
+X_train,X_test,y_train,y_test=train_test_split(X,Y, test_size=0.3, random_state=31)
+lasso = Lasso()
+lasso.fit(X_train,y_train)
+train_score=lasso.score(X_train,y_train)
+test_score=lasso.score(X_test,y_test)
+coeff_used = np.sum(lasso.coef_!=0)
+print ("training score:"), train_score 
+print ("test score: "), test_score
+print ("number of features used: "), coeff_used
 
 
-df=pd.read_csv("train.csv")
-df=pd.DataFrame(df)
-y=df["PE"] 
-x=np.matrix([np.ones(len(y)),df["AT"],df["V"],df["AP"],df["RH"]])
-x=pd.DataFrame(x)
-x=x.T
-a=gradient_descent(x,y)
-print("Coefficients are \n theta :{} ".format(a[2]))
+lasso001 = Lasso(alpha=0.01, max_iter=10e5)
+lasso001.fit(X_train,y_train)
+train_score001=lasso001.score(X_train,y_train)
+test_score001=lasso001.score(X_test,y_test)
+coeff_used001 = np.sum(lasso001.coef_!=0)
+print ("training score for alpha=0.01:"), train_score001 
+print ("test score for alpha =0.01: "), test_score001
+print ("number of features used: for alpha =0.01:"), coeff_used001
 
 
-print("Plot between no of iteration and error")
-plt.plot(a[0],a[1])
+lasso00001 = Lasso(alpha=0.0001, max_iter=10e5)
+lasso00001.fit(X_train,y_train)
+train_score00001=lasso00001.score(X_train,y_train)
+test_score00001=lasso00001.score(X_test,y_test)
+coeff_used00001 = np.sum(lasso00001.coef_!=0)
+print ("training score for alpha=0.0001:"), train_score00001 
+print ("test score for alpha =0.0001: "), test_score00001
+print ("number of features used: for alpha =0.0001:"), coeff_used00001
+
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+lr_train_score=lr.score(X_train,y_train)
+lr_test_score=lr.score(X_test,y_test)
+print ("LR training score:"), lr_train_score 
+print ("LR test score: "), lr_test_score
+
+plt.subplot(1,2,1)
+
+# alpha here is for transparency
+plt.plot(lasso.coef_,alpha=0.7,linestyle='none',marker='*',markersize=5,color='red',label=r'Lasso; $\alpha = 1$',zorder=7) 
+
+# alpha here is for transparency
+plt.plot(lasso001.coef_,alpha=0.5,linestyle='none',marker='d',markersize=6,color='blue',label=r'Lasso; $\alpha = 0.01$') 
+plt.xlabel('Coefficient Index',fontsize=16)
+plt.ylabel('Coefficient Magnitude',fontsize=16)
+plt.legend(fontsize=13,loc=4)
+plt.subplot(1,2,2)
+
+# alpha here is for transparency
+plt.plot(lasso.coef_,alpha=0.7,linestyle='none',marker='*',markersize=5,color='red',label=r'Lasso; $\alpha = 1$',zorder=7) 
+
+# alpha here is for transparency
+plt.plot(lasso001.coef_,alpha=0.5,linestyle='none',marker='d',markersize=6,color='blue',label=r'Lasso; $\alpha = 0.01$') 
+
+# alpha here is for transparency
+plt.plot(lasso00001.coef_,alpha=0.8,linestyle='none',marker='v',markersize=6,color='black',label=r'Lasso; $\alpha = 0.00001$') 
+
+plt.plot(lr.coef_,alpha=0.7,linestyle='none',marker='o',markersize=5,color='green',label='Linear Regression',zorder=2)
+plt.xlabel('Coefficient Index',fontsize=16)
+plt.ylabel('Coefficient Magnitude',fontsize=16)
+plt.legend(fontsize=13,loc=4)
+plt.tight_layout()
 plt.show()
 
-plt.plot(df["AT"],np.transpose(a[3]))
-plt.show()
-#df["AT"]
-
-#source code from https://github.com/sumitsharansatsangi/Lasso_Regression
+#source code from https://towardsdatascience.com/ridge-and-lasso-regression-a-complete-guide-with-python-scikit-learn-e20e34bcbf0b
